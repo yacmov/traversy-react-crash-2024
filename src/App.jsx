d755b15dfc51f5e68abd422f406a3a5e1720c070
Cedric Ko <yacmov@gmail.com>
2024-09-09 03:52:21 +0800

    feat: first commit with structure setup upto Link tag instead of a tag

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route part='/jobs' element={<JobsPage />} />
+    </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
